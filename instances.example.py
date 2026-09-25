9068529bd5ea76664908df904fa9f71220c8321c
dheereshag <da11@iitbbs.ac.in>
2026-09-25 12:53:49 +0530

    change

--- instances.example.py
+++ instances.example.py
@@ -3,14 +3,16 @@
 from coursera_automation.instances import InstanceConfig
 
 INSTANCES: list[InstanceConfig] = [
+    # for first course
     InstanceConfig(
-        email="your_email@example.com",
-        password="your_password",
-        course_url="https://www.coursera.org/specializations/your-course-url",
+        email="24bai71310@cuchd.in",
+        password="Lhya24AI",
+        course_url="https://www.coursera.org/organizations/chandigarh-university/specializations/generative-ai-for-software-developers",
         legal_name="Your Legal Name",
         headless=False,
         max_items=50,
     ),
+    # for second course
     InstanceConfig(
         email="24bai71310@cuchd.in",
         password="Lhya24AI",
